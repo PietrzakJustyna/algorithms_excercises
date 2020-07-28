821c6708f34dd0dcd666233b8568771bee220aa3
Justyna Pietrzak <tynk713@gmail.com>
2020-07-28 08:59:13 +0200

    same amount of same letters

--- some_words_algorithms.py
+++ some_words_algorithms.py
@@ -13,3 +13,14 @@ print(check_if_palindrom("Kajak"))
 print(check_if_palindrom("nie"))
 print(check_if_palindrom("Kobyła ma mały bok"))
 print(check_if_palindrom("Nie jest to palindrom"))
+
+
+print("")
+
+
+def check_if_has_same_letters(word1, word2):
+    return sorted(word1) == sorted(word2)
+
+
+print(check_if_has_same_letters("mama", "aamm"))
+
